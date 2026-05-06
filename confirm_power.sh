c2aab56d731e2595849e9a3df081cac8dd127185
jose <jlfp15@gmail.com>
2026-05-06 20:13:45 -0300

    volvi a waybar

--- confirm_power.sh
+++ confirm_power.sh
@@ -3,12 +3,9 @@
 # Argumentos: $1 = Mensaje para el usuario, $2 = Comando a ejecutar
 
 # Mostramos el menú Sí/No con wofi
-# -dmenu: modo menú
-# -p: prompt personalizado
-# -L 2: solo 2 líneas para que sea compacto
-# -i: insensible a mayúsculas
+# Usamos el tema style3.css para consistencia
 selected=$(echo -e "No
-Sí" | wofi -dmenu -p "$1" -L 2 -i)
+Sí" | wofi -dmenu --style ~/.config/wofi/style3.css -p "$1" -L 2 -i --width 300 --height 150)
 
 if [[ "$selected" == "Sí" ]]; then
     eval "$2"
